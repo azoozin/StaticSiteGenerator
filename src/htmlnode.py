883c194612390895ee3342220a0b9900c55c13a5
azoozin <mario.github@protonmail.com>
2024-09-15 08:35:04 -0300

    add ParentNode class and tests for the class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -23,6 +23,10 @@ class HTMLNode():
         # !r calls __repr__ on each variable
         return f"HTMLNode(tag={self.tag!r}, value={self.value!r}, children={self.children}, props={self.props})"
 
+### CHILD CLASSES ###
+
+# Leaf node has no children
+# A leaf node is a node without children (like leaf in a tree branch)
 class LeafNode(HTMLNode):
     # No children should be set
     # Value must be required
@@ -39,3 +43,38 @@ class LeafNode(HTMLNode):
         opening_tag = f"<{self.tag}{props_string}>"
         closing_tag = f"</{self.tag}>"
         return f"{opening_tag}{self.value}{closing_tag}"
+
+# Parent node has children but no value
+# A node that is not a "leaf node" is a parent node i.e it has children
+# # # (like a branch in a tree with child leaves)
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("No tag found.")
+        if not self.children:
+            raise ValueError("No children found")
+        
+        props_str = ''
+        if hasattr(self, 'props') and self.props:
+            props_str = ' '.join([f'{key}="{value}"' for key, value in self.props.items()])
+
+        complete_html = ''
+        if self.children:
+            html_string_list = []
+            
+            for child in self.children:
+                html_string_list.append(child.to_html())
+
+            concat_html_strings = "".join(html_string_list)
+            complete_html = f"<{self.tag}>{concat_html_strings}</{self.tag}>"
+
+        if props_str:
+            # Include props_str in the opening tag if it exists
+            complete_html = f"<{self.tag} {props_str}>{concat_html_strings}</{self.tag}>"
+        else:
+            complete_html = f"<{self.tag}>{concat_html_strings}</{self.tag}>"
+            
+        return complete_html
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -26,6 +26,7 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(len(node2.children), 1)
         self.assertIsNone(node2.props)
 
+# LeafNode tests
     def test_to_html_no_children(self):
         node = LeafNode("p", "Hello, world!")
         self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
@@ -33,6 +34,65 @@ class TestHTMLNode(unittest.TestCase):
     def test_to_html_no_tag(self):
         node = LeafNode(None, "Hello, world!")
         self.assertEqual(node.to_html(), "Hello, world!")
+
+# ParentNode tests
+    def test_parentnode_tag_and_children(self):
+        children = [
+            LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text")
+        ]
+        node = ParentNode("p", children)
+        expected_html = "<p><b>Bold text</b>Normal text</p>"
+        self.assertEqual(node.to_html(), expected_html)
+
+    def test_parentnode_no_tag(self):
+        children = [
+            LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text")
+        ]
+        node = ParentNode(None, children)
+        with self.assertRaises(ValueError) as context:
+            node.to_html()
+        # check if the raised error msg is the expected one
+        self.assertEqual(str(context.exception), "No tag found.")
+
+    def test_parentnode_empty_children(self):
+        node = ParentNode("p", [])
+        with self.assertRaises(ValueError) as context:
+            node.to_html()
+        self.assertEqual(str(context.exception), "No children found")
+    
+    def test_parentnode_nested_nodes(self):
+        inner_children = [
+            LeafNode("i", "italic text"),
+            LeafNode(None, "normal text")
+        ]
+
+        inner_parent = ParentNode("span", inner_children)
+
+        outer_children = [
+            LeafNode("b", "Bold text"),
+            inner_parent,
+            LeafNode(None, "Additional text")
+        ]
+
+        outer_parent = ParentNode("div", outer_children)
+        
+        expected_html = "<div><b>Bold text</b><span><i>italic text</i>normal text</span>Additional text</div>"
+
+        self.assertEqual(outer_parent.to_html(), expected_html)
+
+    def test_parentnode_props_used(self):
+        children = [
+            LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text")
+        ]
+
+        node = ParentNode("p", children, props={"href": "https://example.com", "target": "_blank"})
+        expected_html = '<p href="https://example.com" target="_blank"><b>Bold text</b>Normal text</p>'
+        self.assertEqual(node.to_html(), expected_html)
+    
+
 
 if __name__ == "__main__":
     unittest.main()
